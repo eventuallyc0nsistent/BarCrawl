c3b711aa3514e839a249a74944a45de7a3c91619
Egor <ekozitski@hotmail.com>
2016-06-05 11:00:13 -0400

    Update index.js

--- BarCrawl/www/js/index.js
+++ BarCrawl/www/js/index.js
@@ -18,47 +18,47 @@
  */
 
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
 
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-   
-      var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
 
-      function success(pos) {
-        var crd = pos.coords;
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    function success(pos) {
+      var crd = pos.coords;
 
 
-        var latitude = crd.latitude;
-        var longitude = crd.longitude;
-       
-        require(["esri/urlUtils",
+      var latitude = crd.latitude;
+      var longitude = crd.longitude;
+
+      require(["esri/urlUtils",
         "esri/config",
         "esri/map",
-        "esri/graphic",            
-        "esri/tasks/RouteTask",            
+        "esri/graphic",
+        "esri/tasks/RouteTask",
         "esri/tasks/RouteParameters",
 
-        "esri/tasks/FeatureSet",            
+        "esri/tasks/FeatureSet",
         "esri/symbols/SimpleMarkerSymbol",
-        "esri/symbols/SimpleLineSymbol",          
+        "esri/symbols/SimpleLineSymbol",
 
         "esri/Color",
         "dojo/_base/array",
@@ -70,99 +70,99 @@ var app = {
         "dijit/layout/BorderContainer",
         "dijit/layout/ContentPane",
         "dijit/form/HorizontalSlider",
-        "dijit/form/HorizontalRuleLabels"], function(
-              urlUtils, esriConfig, Map, Graphic, RouteTask, RouteParameters,
-              FeatureSet, SimpleMarkerSymbol, SimpleLineSymbol,           
-              Color, array, on, dom, registry,OAuthInfo,esriId
-          ){
-      
-           var map = new Map("map", {
-                                      basemap:"streets",
+        "dijit/form/HorizontalRuleLabels"], function (urlUtils, esriConfig, Map, Graphic, RouteTask, RouteParameters,
+                                                      FeatureSet, SimpleMarkerSymbol, SimpleLineSymbol,
+                                                      Color, array, on, dom, registry, OAuthInfo, esriId) {
 
-                                      center:[longitude, latitude],
-                                      zoom: 16
-                                    });
-          var bars = [];
+        var map = new Map("map", {
+          basemap: "streets",
 
-          var routeParams = new RouteParameters();
-              routeParams.stops = new FeatureSet();
-          var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
-          var stopSymbol = new SimpleMarkerSymbol().setStyle(SimpleMarkerSymbol.STYLE_CROSS).setSize(15);
-              stopSymbol.outline.setWidth(3);
-          var routeSymbol = {
-                  "Beer Route": new SimpleLineSymbol().setColor(new Color([0,0,255,0.5])).setWidth(5)
-              };
-              routeParams.outSpartialReference={"wkid":102100};
+          center: [longitude, latitude],
+          zoom: 16
+        });
+        var bars = [];
+
+        var routeParams = new RouteParameters();
+        routeParams.stops = new FeatureSet();
+        var routeTask = new RouteTask("http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
+        var stopSymbol = new SimpleMarkerSymbol().setStyle(SimpleMarkerSymbol.STYLE_CROSS).setSize(15);
+        stopSymbol.outline.setWidth(3);
+        var routeSymbol = {
+          "Beer Route": new SimpleLineSymbol().setColor(new Color([0, 0, 255, 0.5])).setWidth(5)
+        };
+        routeParams.outSpartialReference = {"wkid": 102100};
 
 
-            function addSomeGraphic(){
+        function addSomeGraphic() {
 
-                var url = 'http://localhost:3000/search?latitude='+latitude+'&longitude='+longitude; 
-                jQuery.get(url, function(response){
-                  
-                    //coordinates of coord
-                    var coords = {"geometry":{
-                                    "type":"Point",
-                                    "x":latitude,
-                                    "y":longitude,
-                    }};
-                    var startGraphic = new esri.Graphic(coords, stopSymbol);
-                    startGraphic.symbol = stopSymbol;
-                    bars.push(startGraphic);
+          var url = 'http://localhost:3000/search?latitude=' + latitude + '&longitude=' + longitude;
+          jQuery.get(url, function (response) {
 
-                    response.businesses.forEach(function(business){
+            //coordinates of coord
+            var coords = {
+              "geometry": {
+                "type": "Point",
+                "x": latitude,
+                "y": longitude
+              }
+            };
+            var startGraphic = new esri.Graphic(coords, stopSymbol);
+            startGraphic.symbol = stopSymbol;
+            bars.push(startGraphic);
 
-                        var lat = business.location.coordinate.latitude;
-                        var lon = business.location.coordinate.longitude;
-                        var geom = {
-                            'geometry': {
-                                'type': 'Point',
-                                'x': lon,
-                                'y': lat
-                            }
-                        };
+            response.businesses.forEach(function (business) {
 
-                        var graphic = new esri.Graphic(geom, stopSymbol);
-                        graphic.symbol = stopSymbol;
-                        bars.push(graphic);
+              var lat = business.location.coordinate.latitude;
+              var lon = business.location.coordinate.longitude;
+              var geom = {
+                'geometry': {
+                  'type': 'Point',
+                  'x': lon,
+                  'y': lat
+                }
+              }
+            });
+
+            var graphic = new esri.Graphic(geom, stopSymbol);
+            graphic.symbol = stopSymbol;
+            bars.push(graphic);
 
 
-                        for(var i=0;i<bars.length;++i){
-                          routeParams.stops.features.push(
-                            map.graphics.add(bars[i])
-                          )
-                        }
-                       routeTask.solve(routeParams);
-                    });
-
-                    function clearRoutes(){
-                        for(var i=bars.length-1;i>=0;i--){
-                          map.graphics.remove(bars.splice(i,1)[0]);
-                        }
-                        bars=[];
-                      }
-                      
-                      routeTask.on('solve-complete',showRouter);
-                      function showRouter(evt){
-                          clearRoutes();
-                          array.forEach(evt.result.routeResults,function(routeResult,i){
-                            ///symbol is not setting
-                            bars.push(
-                                map.graphics.add(
-                                  routeResult.route.setSymbol(routeSymbol['Beer Route'])
-                                )
-                              );
-                             console.log(routeResult);
-                          });
-                        
-                      }
-                      
-                });
-
+            for (var i = 0; i < bars.length; ++i) {
+              routeParams.stops.features.push(
+                  map.graphics.add(bars[i])
+              )
             }
-            
-          map.on("load",addSomeGraphic);
-          
+            routeTask.solve(routeParams);
+          });
+
+          function clearRoutes() {
+            for (var i = bars.length - 1; i >= 0; i--) {
+              map.graphics.remove(bars.splice(i, 1)[0]);
+            }
+            bars = [];
+          }
+
+          routeTask.on('solve-complete', showRouter);
+          function showRouter(evt) {
+            clearRoutes();
+            array.forEach(evt.result.routeResults, function (routeResult, i) {
+              ///symbol is not setting
+              bars.push(
+                  map.graphics.add(
+                      routeResult.route.setSymbol(routeSymbol['Beer Route'])
+                  )
+              );
+              console.log(routeResult);
+            });
+
+          }
+
+        };
+
+
+        map.on("load", addSomeGraphic);
+
 
       });
 
@@ -171,18 +171,17 @@ var app = {
       };
 
       // get current location and show on map
-      navigator.geolocation.getCurrentPosition(success, error, options);
-        
-    },
+    }
+  },
 
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var listeningElement = $(id + ' .listening');
-        var receivedElement = $(id + ' .received');
-        listeningElement.hide();
-        receivedElement.show();
+      var listeningElement = $(id + ' .listening');
+      var receivedElement = $(id + ' .received');
+      listeningElement.hide();
+      receivedElement.show();
     }
-};
+  };
 
 app.initialize();
